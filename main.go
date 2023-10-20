9065fbd39b130dca9d6fab56f9a24845c2928afc
Hongwei Han <louis.niuniu@gmail.com>
2023-10-20 08:17:32 +0200

    add code

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"DemogRPC/service"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+)
+
+func main() {
+
+	s := &service.User{
+		Username: "niuniu",
+		Age:      12,
+	}
+
+	b, err := proto.Marshal(s)
+	if err != nil {
+		fmt.Println("error info:", err)
+	}
+
+	fmt.Println(b)
+
+	newUser := &service.User{}
+	err1 := proto.Unmarshal(b, newUser)
+	if err1 != nil {
+		fmt.Println("unmarshal error is ", err1)
+	}
+
+}
